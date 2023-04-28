bc6dc97b43b6ac8bccd24bf5f942cd5875be13f7
Arthur <aking4511@gmail.com>
2023-04-29 02:20:04 +0800

    feat: add update restaurant function

--- app.js
+++ app.js
@@ -52,11 +52,40 @@ app.post('/restaurants', (req, res) => {
 
 //show pages(should be lower)
 app.get('/restaurants/:id', (req, res) => {
-    const id = req.params.id
-    return Restaurants.findById(id)
-      .lean()
-      .then((restaurant) => {res.render('show', {restaurant})})
-      .catch(error => console.error(error))
+  const id = req.params.id
+  return Restaurants.findById(id)
+    .lean()
+    .then((restaurant) => {res.render('show', {restaurant})})
+    .catch(error => console.error(error))
+})
+
+//edit page
+app.get('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Restaurants.findById(id)
+    .lean()
+    .then((restaurant) => {res.render('edit', {restaurant})})
+    .catch(error => console.error(error))
+})
+
+app.post('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  const {name,name_en,category,image,location,phone,google_map,rating,description} = req.body
+  return Restaurants.findById(id)
+    .then((restaurant) => {
+      restaurant.name = name
+      restaurant.name_en = name_en
+      restaurant.category = category
+      restaurant.image = image
+      restaurant.location = location
+      restaurant.phone = phone
+      restaurant.google_map
+      restaurant.rating = rating
+      restaurant.description = description
+      return restaurant.save()
+    })
+    .then((restaurant) => {res.redirect(`/restaurants/${id}`)})
+    .catch(error => console.error(error))
 })
 
 //search function
