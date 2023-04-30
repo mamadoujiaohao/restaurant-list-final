e68cd881e7126c3d4bea2ed12331e9fd9e01133e
Arthur <aking4511@gmail.com>
2023-04-30 16:13:09 +0800

    feat: edit .env & fix login alert message

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
+const FacebookStrategy = require('passport-facebook').Strategy
 const User = require('../models/user') 
 const bcrypt = require('bcryptjs')
 
@@ -7,21 +8,48 @@ module.exports = app => {
     app.use(passport.initialize())
     app.use(passport.session())
 
-    passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+    //local strategy
+    passport.use(new LocalStrategy({ usernameField: 'email', passReqToCallback: true }, (req, email, password, done) => {
       User.findOne({ email })
         .then(user => {
           if (!user) {
-            return done(null, false, { message: 'That email is not registered!' })
+            return done(null, false, req.flash('warning_msg', '這Email沒有被註冊過。'))
           }
           return bcrypt.compare(password, user.password).then(isMatch => {
             if (!isMatch) {
-              return done(null, false, { message: 'Email or Password incorrect.' })
+              return done(null, false, req.flash('warning_msg', 'Email 或 Password 錯誤。'))
             }
             return done(null, user)
           })
         })
         .catch(err => done(err, false))
     }))
+
+    //facebook strategy
+    passport.use(new FacebookStrategy({
+      clientID: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+      callbackURL: process.env.FACEBOOK_CALLBACK,
+      profileFields: ['email', 'displayName']
+    }, (accessToken, refreshToken, profile, done) => {
+      const { name, email } = profile._json
+      User.findOne({ email })
+        .then(user => {
+          if (user) return done(null, user)
+          const randomPassword = Math.random().toString(36).slice(-8)
+          bcrypt
+            .genSalt(10)
+            .then(salt => bcrypt.hash(randomPassword, salt))
+            .then(hash => User.create({
+              name,
+              email,
+              password: hash
+            }))
+            .then(user => done(null, user))
+            .catch(err => done(err, false))
+        })
+    }))
+
     passport.serializeUser((user, done) => {
         done(null, user.id)
     })
