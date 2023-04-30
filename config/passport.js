b0dc4399b6d9526ceba348854f8e8d9f0f5ec7f3
Arthur <aking4511@gmail.com>
2023-04-30 15:46:27 +0800

    feat: add bcrypt to login/register  route

--- config/passport.js
+++ config/passport.js
@@ -1,30 +1,27 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const User = require('../models/user') 
+const bcrypt = require('bcryptjs')
 
 module.exports = app => {
     app.use(passport.initialize())
     app.use(passport.session())
 
-    passport.use(new LocalStrategy({ usernameField: 'email' ,passReqToCallback: true}, (req, email, password, done) => {
-        User.findOne({ email })
-          .then(user => {
-            if(email.length ===0 ){
-              return done(null, false, req.flash("warning_msg", "email?!"))
-            }
-
-
-            if (!user) {
-              return done(null, false, req.flash("warning_msg", "This email is not registered!"))
-            }
-            if (user.password !== password) {
-              return done(null, false, req.flash("warning_msg", "email or password incorrect!"))
+    passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+      User.findOne({ email })
+        .then(user => {
+          if (!user) {
+            return done(null, false, { message: 'That email is not registered!' })
+          }
+          return bcrypt.compare(password, user.password).then(isMatch => {
+            if (!isMatch) {
+              return done(null, false, { message: 'Email or Password incorrect.' })
             }
             return done(null, user)
           })
-          .catch(err => done(err, false))
-      }))
-
+        })
+        .catch(err => done(err, false))
+    }))
     passport.serializeUser((user, done) => {
         done(null, user.id)
     })
