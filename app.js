07bd509283679ea15e050f6a2959b06b471a07cd
Arthur <aking4511@gmail.com>
2023-04-30 15:35:25 +0800

    feat: add alert message to login/register page & partials(didn't work)

--- app.js
+++ app.js
@@ -8,13 +8,15 @@ const methodOverride = require('method-override')
 const session = require('express-session')
 const usePassport = require('./config/passport')
 const flash = require('connect-flash')
+//const validator = require('express-validator')
 
 const routes = require('./routes')//express自動找index.js
 require('./config/mongoose')
 
 // setting template engine
-app.engine('hbs', exphbs({defaultLayout: 'main'}))
+app.engine('hbs', exphbs({defaultLayout: 'main' , partialsDir: 'views/partials'}))
 app.set('view engine', 'hbs')
+
 
 //define server related variables
 const port = process.env.PORT
@@ -26,12 +28,15 @@ app.use(methodOverride('_method'))
 app.use(session({secret: process.env.SESSION_SECRET, resave: false, saveUninstalize: true}))
 usePassport(app)
 
-
+app.use(flash())
 app.use((req, res, next) => {
-    res.locals.isAuthenticated = req.isAuthenticated()
-    res.locals.user = req.user
-    next()
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
+  next()
 })
+
   
 //routes
 app.use(routes)
--- config/passport.js
+++ config/passport.js
@@ -6,14 +6,19 @@ module.exports = app => {
     app.use(passport.initialize())
     app.use(passport.session())
 
-    passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+    passport.use(new LocalStrategy({ usernameField: 'email' ,passReqToCallback: true}, (req, email, password, done) => {
         User.findOne({ email })
           .then(user => {
+            if(email.length ===0 ){
+              return done(null, false, req.flash("warning_msg", "email?!"))
+            }
+
+
             if (!user) {
-              return done(null, false, { message: 'That email is not registered!' })
+              return done(null, false, req.flash("warning_msg", "This email is not registered!"))
             }
             if (user.password !== password) {
-              return done(null, false, { message: 'Email or Password incorrect.' })
+              return done(null, false, req.flash("warning_msg", "email or password incorrect!"))
             }
             return done(null, user)
           })
